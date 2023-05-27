a2bf280cb760a742986fa9629617033825e0df1f
Vipm27 <86101819+Vipm27@users.noreply.github.com>
2023-05-27 22:01:11 +0300

    My Work

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,23 +1,29 @@
 import { useState, useEffect } from 'react';
 import { Layout, Row, Col } from 'antd';
-import Movie from '../components/Movie';
+import Movies from '../components/Movies';
 import Search from '../components/Search';
 
 const Main = () => {
   const [movies, setMovies] = useState([]);
-
+  const [value, setValue] = useState('terminator');
   useEffect(() => {
-    fetch(`http://www.omdbapi.com/?apikey=c8ed8ebe&s={}&page=1`)
+    if (value) {
+      fetch("http://www.omdbapi.com/?apikey=c8ed8ebe&s="+(value))
+        .then(res => res.json())
+        .then(data => {
+          if (data.Response === "True") {
+            setMovies(data.Search);
+          } else {
+            setMovies([]);
+          }
+        })
+    }else{
+      fetch(`http://www.omdbapi.com/?apikey=c8ed8ebe&s=terminator&page=1`)
       .then(res => res.json())
       .then(data => setMovies(data.Search))
-  }, []);
+    }
+  }, [value]);
 
-  const [value, setValue] = useState('')
-
-  const filteredPlants = movies.filter( movie => {
-      return movie.Title.toLowerCase().includes(value.toLowerCase())
-  })
-  
   return (
     <>
     <Search onChangeFunc={(event) => setValue(event.target.value)}/>
@@ -25,14 +31,11 @@ const Main = () => {
       <Row>
         <Col xs={24} sm={{ span: 18, offset: 3 }}>
           <Row className='justify-center' gutter={[32, 32]}>
-                {
-                    filteredPlants.map((movie) => {
-                        return(
-                            <Movie key={movie.imdbID} {...movie} />
-                        )
-                    })
-                }
-
+            {
+              movies.length ?
+                <Movies movies={movies} />
+                : <h1>Loading...</h1>
+            }
           </Row>
         </Col>
       </Row>
